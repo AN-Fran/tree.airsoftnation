d649a4238cb866ab5b27a5002f824839481e7d60
AN-Fran <buzon03@xilae.com>
2026-02-20 17:56:02 +0000

    restore wolverine section

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,14 @@
-const CACHE_NAME = "airsoftnation-v3";
-
-const STATIC_ASSETS = [
-  "/",
-  "/manifest.json",
-  "/logo_airsoft_nation.svg",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
+const CACHE_NAME = "airsoftnation-v4";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+        keys.map(key => caches.delete(key))
       )
     )
   );
@@ -29,8 +17,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
