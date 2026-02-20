cf8958f23b03b870d1c5d7d38b97430d78f39a5e
AN-Fran <buzon03@xilae.com>
2026-02-20 17:01:41 +0000

    App

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "airsoftnation-v2";
+
+const STATIC_ASSETS = [
+  "/",
+  "/manifest.json",
+  "/logo_airsoft_nation.svg",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
